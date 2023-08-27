b85f69f4becf4128842b3b70031ebefb746037e0
hkvil <elbert.herry11@gmail.com>
2023-08-27 19:09:44 +0700

    Next:Setup ViewModel LiveData,RecyclerView Adapte.

---
+++ app/src/main/java/com/example/githubapisubmission/data/UserViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.githubapisubmission.data
+
+import androidx.lifecycle.ViewModel
+
+class UserViewModel :ViewModel() {
+   //SEARCH -> FOTO,USERNAME
+    //DETAIL -> FOTO,USERNAME,NAMA,JUMLAH FOLLOWER,JUMLAH FOLLOWING
+
+}
